27d4059d064012d0a305c8f081e360bda131c59e
Pratik Patel <pratiikpatel.001@gmail.com>
2026-06-22 07:57:54 +0545

    loginflow setup

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,24 +1,41 @@
+import { useForm } from "react-hook-form";
+import type { SubmitHandler } from "react-hook-form";
+
+type LoginInput = {
+  Email: string;
+  password: string;
+};
+
 export default function LoginForm() {
+  const { register, handleSubmit } = useForm<LoginInput>();
+
+  const onsubmit: SubmitHandler<LoginInput> = (data) => console.log(data);
+
   return (
     <>
-      <div>
-        <input
-          placeholder="Email"
-          className="w-full border rounded px-3 py-2"
-        />
-      </div>
+      <form onSubmit={handleSubmit(onsubmit)}>
+        <div>
+          <input
+            placeholder="Email"
+            type="email"
+            {...register("Email")}
+            className="w-full border rounded px-3 py-2"
+          />
+        </div>
 
-      <div>
-        <input
-          type="password"
-          placeholder="Password"
-          className="w-full border rounded px-3 py-2"
-        />
-      </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            {...register("password")}
+            className="w-full border rounded px-3 py-2"
+          />
+        </div>
 
-      <button type="submit" className="bg-blue-600 text-white rounded py-2">
-        Submit
-      </button>
+        <button type="submit" className="bg-blue-600 text-white rounded py-2">
+          Submit
+        </button>
+      </form>
     </>
   );
 }
