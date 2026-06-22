833b8978b84d58afd51dc932e92aee88fd0d3208
Pratik Patel <pratiikpatel.001@gmail.com>
2026-06-22 14:32:16 +0545

    implement signin page UI

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -22,15 +22,15 @@ export default function LoginForm() {
   return (
     <>
       {/* Main container */}
-      <div className="relative ">
+      <div className="w-full">
         {/* top-left design */}
-        <div className="absolute top-0 left-0  rounded-xl bg-surface-blur w-96 h-96"></div>
+        {/* <div className=" top-0 left-0  rounded-xl bg-surface-blur w-96 h-96"></div> */}
 
-        {/* Form Container */}
-        <div className="flex flex-col gap-stack-lg max-w-[440]">
-          {/* form design */}
-          <div className="flex flex-col pb-stack-sm">
-            <img src={mailicon} alt="mailicon" className="w-16" />
+        {/* Content Container */}
+        <div className="flex flex-col gap-stack-lg max-w-110 mx-auto">
+          {/* form Title */}
+          <div className="flex flex-col pb-stack-sm mx-auto">
+            <img src={mailicon} alt="mailicon" className="w-16 mx-auto" />
             <span className="text-on-surface font-sans text-center tracking-[-0.6px] text-headline-md">
               Mail Tracko
             </span>
@@ -39,114 +39,126 @@ export default function LoginForm() {
             </span>
           </div>
 
-          <div className="rounded-lg border border-[#CDC6B24D] shadow-[0px_8px_30px_0px_#0000000A] p-8 bg-[#FFFFFF] flex flex-col gap-4">
-            <div className="w-109.5 h-1 bg-[#544600]"></div>
+          {/* form container */}
+          <div className="relative">
+            <div className="w-109.5 h-1 bg-[#544600] absolute left-0"></div>
+            <div className="w-full rounded-lg border border-[#CDC6B24D] shadow-[0px_8px_30px_0px_#0000000A] p-8 bg-[#FFFFFF] flex flex-col gap-4">
+              {/* Welcome div */}
+              <div className="flex flex-col gap-1">
+                <span className="text-title-sm text-on-surface text-center">
+                  Welcome back
+                </span>
+                <span className="text-body-sm text-center text-[#4B4738]">
+                  Please enter your details to sign in.
+                </span>
+              </div>
 
-            <div className="flex flex-col gap-1">
-              <span className="text-title-sm text-on-surface text-center">
-                Welcome back
-              </span>
-              <span className="text-body-sm text-center text-[#4B4738]">
-                Please enter your details to sign in.
-              </span>
-            </div>
-
-            <div className="pt-stack-sm">
-              <form onSubmit={handleSubmit(onsubmit)}>
-                <div className="flex flex-col gap-y-stack-md">
-                  <div className="flex flex-col gap-stack-sm">
-                    <label
-                      htmlFor="email"
-                      className="text-[#4B4738] text-label-caps"
-                    >
-                      Work Email
-                    </label>
-                    <div className="flex items-center border border-[#CDC6B280] bg-surface-container-low rounded-DEFAULT py-3.5 pl-10 pr-4">
-                      <img
-                        src={emailcontainer}
-                        alt="EmailContainerIcon"
-                        className="w-3 h-5"
-                      />
-                      <input
-                        placeholder="name@company.com"
-                        type="email"
-                        {...register("email")}
-                        className=" text-[#CDC6B2B2] leading-[100%] font-normal text-[16px] text-center"
-                      />
-                    </div>
-                  </div>
-
-                  <div className="flex flex-col gap-stack-sm">
-                    <div className="w-full justify-between items-center">
+              {/* Form Content  */}
+              <div className="pt-stack-sm">
+                <form onSubmit={handleSubmit(onsubmit)}>
+                  <div className="flex flex-col gap-stack-md">
+                    <div className="flex flex-col gap-stack-sm">
                       <label
-                        htmlFor="password"
+                        htmlFor="email"
                         className="text-[#4B4738] text-label-caps"
                       >
-                        Password
+                        Work Email
                       </label>
-                      <span className="text-[#544600] text-label-caps">
-                        Forgot password?
-                      </span>
+                      <div className="w-full flex gap-2 items-center border border-[#CDC6B280] bg-surface-container-low rounded-DEFAULT py-3.5 pl-10 pr-4">
+                        <img
+                          src={emailcontainer}
+                          alt="EmailContainerIcon"
+                          className="w-[16.67px] h-5"
+                        />
+                        <input
+                          placeholder="name@company.com"
+                          type="email"
+                          {...register("email")}
+                          className=" text-[#CDC6B2B2] leading-[100%] font-normal text-[16px]"
+                        />
+                      </div>
                     </div>
-                    <div className="relative flex items-center border border-[#CDC6B280] bg-surface-container-low rounded-DEFAULT py-3.5 pl-10 pr-4">
-                      <img
-                        src={password}
-                        alt="PasswordIcon"
-                        className="w-3 h-5"
-                      />
-                      <input
-                        type="password"
-                        placeholder="••••••••"
-                        {...register("password")}
-                        className="text-[#CDC6B2B2] font-normal text-[16px] leading-[100%] text-center"
-                      />
-                      <img
-                        src={eyebutton}
-                        alt="EyeIcon"
-                        className="w-4.5 h-4 pt-[3.63px] pb-[0.37px] absolute right-4"
-                      />
-                    </div>
-                  </div>
 
-                  <div className="pt-stack-sm">
-                    <button
-                      type="submit"
-                      className=" flex gap-stack-sm rounded-DEFAULT bg-primary py-3.5 text-surface-container-lowest text-title-sm text-center"
-                    >
-                      Sign In
-                      <img src={right} alt="RightIcon" className="w-3" />
-                    </button>
+                    <div className="flex flex-col gap-stack-sm">
+                      <div className="w-full flex justify-between items-center">
+                        <label
+                          htmlFor="password"
+                          className="text-[#4B4738] text-label-caps"
+                        >
+                          Password
+                        </label>
+
+                        <span className="text-[#544600] text-label-caps">
+                          Forgot password?
+                        </span>
+                      </div>
+                      <div className="relative flex gap-2 items-center border border-[#CDC6B280] bg-surface-container-low rounded-DEFAULT py-3.5 pl-10 pr-4">
+                        <img
+                          src={password}
+                          alt="PasswordIcon"
+                          className="w-[13.33] h-5"
+                        />
+                        <input
+                          type="password"
+                          placeholder="••••••••"
+                          {...register("password")}
+                          className="text-[#CDC6B2B2] font-normal text-[16px] leading-[100%] "
+                        />
+                        <img
+                          src={eyebutton}
+                          alt="EyeIcon"
+                          className="w-4.5 h-4 pt-[3.63px] pb-[0.37px] absolute right-4"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="pt-stack-sm">
+                      <button
+                        type="submit"
+                        className="w-full flex justify-center gap-stack-sm rounded-DEFAULT bg-primary py-3.5 text-surface-container-lowest text-title-sm shadow-button"
+                      >
+                        Sign In
+                        <img src={right} alt="RightIcon" className="w-3" />
+                      </button>
+                    </div>
                   </div>
-                </div>
-              </form>
-            </div>
-            <div className="py-stack-sm flex items-center">
-              <div className="w-[106.61px] h-px border border-t-[#CDC6B24D]"></div>
-              <span className="px-stack-md text-[#CDC6B2] text-label-caps">
-                OR CONTINUE WITH
-              </span>
-              <div className="w-[106.63px] h-px border border-t-[#CDC6B24D]"></div>
-            </div>
-            <div className="flex gap-stack-sm">
-              <div className="rounded-DEFAULT border border-[#CDC6B280] bg-surface-container-lowest flex gap-3 py-2.5 pr-[54.03px] pl-[54.02px]">
-                <img src={google} alt="GoogleIcon" className="w-4 h-4" />
-                <span className="text-[#4B4738] text-label-caps">Google</span>
+                </form>
               </div>
-              <div className="rounded-DEFAULT border border-[#CDC6B280] bg-surface-container-lowest flex gap-3 py-2.5 pr-[46.12px] pl-[46.11px]">
-                <img src={microsoft} alt="MicrosoftIcon" className="w-4 h-4" />
-                <span className="text-[#4B4738] text-label-caps">
-                  Microsoft
+
+              {/* Form outside container */}
+              <div className=" w-full py-stack-sm flex items-center ">
+                <div className="w-[106.61px] h-px border border-t-[#CDC6B24D]"></div>
+                <span className="px-3.5 text-[#CDC6B2] text-label-caps">
+                  OR CONTINUE WITH
                 </span>
+                <div className="w-[106.63px] h-px border border-t-[#CDC6B24D]"></div>
+              </div>
+              <div className="flex gap-stack-sm">
+                <div className="rounded-DEFAULT border border-[#CDC6B280] bg-surface-container-lowest flex gap-3 py-2.5 pr-[54.03px] pl-[54.02px]">
+                  <img src={google} alt="GoogleIcon" className="w-4 h-4" />
+                  <span className="text-[#4B4738] text-label-caps">Google</span>
+                </div>
+                <div className="rounded-DEFAULT border border-[#CDC6B280] bg-surface-container-lowest flex gap-3 py-2.5 pr-[46.12px] pl-[46.11px]">
+                  <img
+                    src={microsoft}
+                    alt="MicrosoftIcon"
+                    className="w-4 h-4"
+                  />
+                  <span className="text-[#4B4738] text-label-caps">
+                    Microsoft
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-          <div className="text-[#4B4738]">
+
+          <div className="text-[#4B4738] flex justify-center items-center gap-1">
             <span className="text-body-sm">Don't have an account? </span>
             <span className="font-bold text-[16px] leading-6">
               Create an account
             </span>
           </div>
-          <div className="py-stack-md">
+          <div className="py-stack-md flex justify-center">
             <div className="flex gap-stack-md">
               <span className="text-[#7C7766] text-label-caps">
                 Privacy Policy
